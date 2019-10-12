b7e9f195b2fcde7ed9f78695e470940c2ccc9498
sku77 <ezrajohnasy@gmail.com>
2019-10-12 22:29:33 +0800

    Displaying data records.

--- Package.py
+++ Package.py
@@ -3,6 +3,7 @@ from record import Record
 class Package:
 
     def __init__(self):
+        self.event_id = ''
         self.event_name = ''
         self.location = ''
         self.duration_hours = ''
@@ -31,3 +32,17 @@ class Package:
         query = "insert into packages(location, duration, head_rate, inclusion, event_name)" \
                 "VALUES('{0}','{1}','{2}','{3}','{4}');".format(self.location, self.duration_hours, self.head_rate, self.inclusion, self.event_name)
         Record.runQuery(query)
+
+    @staticmethod
+    def getbyID(packageID):
+        query = "select * from packages where package_id = ('{}');".format(packageID)
+        packageRecord = Record.fetchSingleRecord(query)
+        package = Package()
+        package.event_id = packageRecord[0]
+        package.location = packageRecord[1]
+        package.duration_hours = packageRecord[2]
+        package.head_rate = packageRecord[3]
+        package.inclusion = packageRecord[4]
+        package.event_name = packageRecord[5]
+
+        return package
--- User.py
+++ User.py
@@ -1,5 +1,5 @@
 from record import Record
-
+import os
 class User:
     def __init__(self):
         self.user_id = ''
@@ -23,9 +23,10 @@ class User:
 
     def setContact(self, args):
         self.contact_number = args
-
+    
     def setFullname(self):
         self.full_name = self.first_name + ' ' + self.last_name
+        return self.full_name
 
     def save(self):
         query = "insert into users(first_name, last_name, address, contact_number)" \
@@ -35,8 +36,42 @@ class User:
     @staticmethod
     def getbyID(userID):
         query = "select * from users where user_id = ('{}');".format(userID)
-        userRecord = Record.fetchRecord(query)
-        return userRecord
+        userRecord = Record.fetchSingleRecord(query)
+        model = User()
+        model.user_id = userRecord[0]
+        model.first_name = userRecord[1]
+        model.last_name = userRecord[2]
+        model.address = userRecord[3]
+        model.contact_number = userRecord[4]
+        model.full_name = model.setFullname()
+
+        return model
+
+    @staticmethod
+    def getRecords():
+        query = "select * from users;"
+        userRecords = Record.fetchAllRecord(query)
+
+        return userRecords
+
+    @staticmethod
+    def printUserRecords():
+        Records = User.getRecords()
+        os.system('clear')
+
+        print("-----------------------------------------------------USER RECORDS------------------------------------------------------------")
+        for row in Records:
+            print('| ID: {} | First Name: {} | Last Name: {} | Address: {} | Phone: {} |'.format(row[0],row[1],row[2],row[3],row[4]))
+            print("-----------------------------------------------------------------------------------------------------------------------------")
+
+
+
+
+
+
+
+
+
 
 
 
